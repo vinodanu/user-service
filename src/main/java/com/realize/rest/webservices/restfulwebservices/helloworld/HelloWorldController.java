69f80a06717dbd0ca29bc63005864445dd793927
vinodanu <anantavinodkumar@gmail.com>
2024-05-04 12:41:58 +0530

    added internalization to the hellow world controller

--- src/main/java/com/realize/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/realize/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.realize.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //RestAPI
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     //hello-world
     @GetMapping(path = "/hello-world")
@@ -21,5 +31,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World!!!, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internalized")
+    public String helloWorldInternalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
